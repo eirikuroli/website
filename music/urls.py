d2d345d6008198467336f4de30006d901dfb39d6
eirikuroli <eirikuroli@gmail.com>
2018-05-14 12:54:00 +0000

    Seinasti dagur i fb

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'music'
+
 urlpatterns = [
     # /music/
-    url(r'^$',views.index, name='index'),
+    url(r'^$',views.IndexView.as_view(), name='index'),
 
-    # /music/712/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /music/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
