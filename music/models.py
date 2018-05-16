6a502b281a5d68ecdcd5e01d700287591f1d63ec
eirikuroli <eirikuroli@gmail.com>
2018-05-16 15:19:49 +0000

    Bull

--- music/models.py
+++ music/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 #Red pk 1
 class Album(models.Model):
@@ -6,6 +8,9 @@ class Album(models.Model):
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
 
 
     def __str__(self):
--- music/views.py
+++ music/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Album
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -12,3 +14,15 @@ class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
 
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist','album_title', 'genre', 'album_logo']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
